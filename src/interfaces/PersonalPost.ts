d51873a450b7fbbd5f49ec5601718fa5ab99131f
it's U <40010878+j2woo@users.noreply.github.com>
2023-01-07 17:22:26 +0900

    Update PersonalPost.ts

--- src/interfaces/PersonalPost.ts
+++ src/interfaces/PersonalPost.ts
@@ -9,8 +9,8 @@ export interface PersonalPost {
   personal_post_id: string;
   personal_rolling_paper_id: string;
   user_id: string | null;
-  content: string;
-  anonymous_type: AnonymousType;
+  content: string | null;
+  anonymous_type: AnonymousType | null;
   post_color: string;
   emoji_type: string | null;
   non_member_password: string | null;
@@ -23,8 +23,8 @@ export interface PersonalPostInputDTO {
 export interface PersonalPostDTO {
   personal_rolling_paper_id: string;
   user_id: string | null;
-  content: string;
-  anonymous_type: AnonymousType;
+  content: string | null;
+  anonymous_type: AnonymousType | null;
   post_color: string;
   non_member_password: string | null;
 }
